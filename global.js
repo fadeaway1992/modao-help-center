296275939d39cc5094631e85c7ad19c10db968bd
fadeaway1992 <498497303@qq.com>
2019-08-22 16:54:43 +0800

    something

--- global.js
+++ global.js
@@ -1,1 +1,18 @@
-$('body>div:first-child').remove()
+// // 进入页面后删除原生头部
+// $('body>div:first-child').remove()
+
+// // 设置语言
+// (function(){
+//   if (location.search.indexOf('lang=en') !== -1) {
+//     if(document.querySelector('.switch-lang .en').classList.contains('active')) return
+//     document.querySelectorAll('.switch-lang a').forEach(function (element)  {
+//       element.classList.remove('active')
+//     })
+//     document.querySelector('.switch-lang .en').classList.add('active')
+//   }
+// })()
+
+
+$('.cate-wrap .toggle-icon').click(function(e) {
+  e.target.parentNode.parentNode.classList.toggle('open')
+})
