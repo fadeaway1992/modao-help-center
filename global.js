fce9a4a24b83eadd915be9e19f426828c644b194
fadeaway1992 <498497303@qq.com>
2019-08-22 20:23:11 +0800

    something

--- global.js
+++ global.js
@@ -16,3 +16,19 @@
 $('.cate-wrap .toggle-icon').click(function(e) {
   e.target.parentNode.parentNode.classList.toggle('open')
 })
+
+// 点击播放视频
+function playVideo (e) {
+  const selector = e.target.dataset.play
+  const videoCover = document.querySelector(`#${selector}`)
+  videoCover.style.display = 'flex'
+  videoCover.querySelector('video').play()
+}
+
+// 停止视频播放
+function closeVideo(e) {
+  const videoBox = e.target.parentNode.parentNode
+  const video = videoBox.querySelector('video')
+  video.pause()
+  e.target.parentNode.parentNode.style.display = 'none'
+}
